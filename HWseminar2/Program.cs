1824e789b1f5e4be0055369968c59d8755ae459d
Guccher <Guccher_007@mail.ru>
2022-08-13 18:29:00 +0300

    Did task № 2

--- HWseminar2/Program.cs
+++ HWseminar2/Program.cs
@@ -1,11 +1,23 @@
 ﻿// задача № 1
 
 // Console.Write("Input Three-Digit Number: ");
-// int ThreeDigitNumber = Convert.ToInt32(Console.ReadLine());
-// int SecondNumber = 0;
-// SecondNumber = ThreeDigitNumber % 100 / 10;
-// Console.WriteLine("Second Number = " + SecondNumber);
+// int ThreeDigitNumber1 = Convert.ToInt32(Console.ReadLine());
+// int SecondDigit = 0;
+// SecondDigit = ThreeDigitNumber % 100 / 10;
+// Console.WriteLine("Second Digit = " + SecondDigit);
 
 
 // задача № 2
+
+// Console.Write("Input Number : ");
+// int Number = Convert.ToInt32(Console.ReadLine());
+// int ThirdDigit = 0;
+// if (Number > 99){
+//     Console.WriteLine(Number.ToString ()[2]);
+// }
+// else {
+//     Console.WriteLine("No Third Digit");
+// }
+
+
 
