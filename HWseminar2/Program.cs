0e21ea307f70712e7ca2fa209d4c2bc7366c5261
Guccher <Guccher_007@mail.ru>
2022-08-13 16:20:13 +0300

    Did task № 1

---
+++ HWseminar2/Program.cs
@@ -0,0 +1,11 @@
+﻿// задача № 1
+
+// Console.Write("Input Three-Digit Number: ");
+// int ThreeDigitNumber = Convert.ToInt32(Console.ReadLine());
+// int SecondNumber = 0;
+// SecondNumber = ThreeDigitNumber % 100 / 10;
+// Console.WriteLine("Second Number = " + SecondNumber);
+
+
+// задача № 2
+
